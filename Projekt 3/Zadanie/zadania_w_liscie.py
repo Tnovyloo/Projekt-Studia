492d2529c8ca1ebf63008377efd57e64751a23e7
TnovyLoo <loobrome@gmail.com>
2023-01-19 18:19:19 +0100

    Zrobienie reszty zadań poprawnie

--- Projekt 3/Zadanie/zadania_w_liscie.py
+++ Projekt 3/Zadanie/zadania_w_liscie.py
@@ -74,7 +74,9 @@ graf4 = [['A', 'B'],
          ['F', 'I'],
          ['G', 'H'],
          ['G', 'I'],
-         ['H', 'I']
+         ['H', 'I'],
+         ['J', 'J'],
+         # ['H', 'J']
          ]
 
 
@@ -87,7 +89,9 @@ graf5 = [['A', 'B'],
          ['D', 'E'],
          ['E', 'F'],
          ['F', 'G'],
-         ['G', 'H']
+         ['G', 'H'],
+         # TEST
+         ['D', 'B']
          ]
 
 # FUNKCJE DO KONKRETNYCH ZADAŃ
@@ -161,15 +165,16 @@ def zadanie2(graph: list):
         print(i)
 
 
-def zadanie3(graf):
+def zadanie3(graph):
     """Funkcja pokazujaca wszystkie stopnie wychodzące wszystkich wierzchołków"""
 
     return_list = []
 
-    for e in graf:
+    for e in graph:
         v1 = e[0]
         help_list = []
-        for e1 in graf:
+        for e1 in graph:
+            # Jeśli wierzchołek 'v1' zawiera się w elemencie listy krawędzi (0 miejsec) to je dodaj
             if v1 in e1[0]:
                 help_list.append([e1[0], e1[1]])
 
@@ -182,23 +187,109 @@ def zadanie3(graf):
         print(i)
 
 
-def zadanie4(graf):
-    pass
+def zadanie4(graph):
+    """Funkcja wypisujaca wszystkie stopnie wchodzace do wierzcholków"""
+    return_list = []
 
-def zadanie5(graf):
-    pass
+    for e in graph:
+        # Bierzemy drugi wierzcholek z listy krawedzi
+        v1 = e[1]
+        help_list = []
+        for e1 in graph:
+            # Jesli w elemencie listy krawedzi na drugim miejscu zawiera sie ten sam wierzchołek to go dodaj.
+            if v1 in e1[1]:
+                help_list.append([e1[0], e1[1]])
+        # Pomiń duplikaty
+        text = [f"Stopnie wchodzące do wierzchołka {v1}:", help_list]
+        if text not in return_list:
+            return_list.append([f"Stopnie wchodzące do wierzchołka {v1}:", help_list])
 
-def zadanie6(graf):
-    pass
+    for i in return_list:
+        print(i)
 
-def zadanie7(graf):
+
+def zadanie5(graph):
+    """Funkcja wypisująca wierzchołki izolowane.
+        (Czyli w sumie takie które tworzą pętle z samym sobą,
+        oraz nie są i nie mają sąsiadów)"""
+
+    return_list = []
+
+    for e in graph:
+        help_list = []
+        v1 = e[0]
+        v2 = e[1]
+        # Sprawdzamy czy dwa wierzchołki są takie same w tym elemencie listy
+        if v1 == v2:
+            # Wchodzimy w petle ktora sprawdzi czy nie ma innych krawedzi,
+            # które zawierają wierzchołek
+            for e1 in graph:
+                if e1[0] == v1 or e1[1] == v1 is False:
+                    help_list.append(e)
+                else:
+                    help_list = []
+        # Jeśli lista nie jest pusta to ją dodaj do listy zwracanej.
+        if help_list:
+            return_list.append(help_list)
+
+    for i in return_list:
+        print(i)
+
+
+def zadanie6(graph):
+    """
+    Funkcja wypisująca wszystkie pętle
+    """
+
+    # NIE UDANA PRÓBA
+    # edges_len = len(graph)
+    #
+    # for e in graph:
+    #     print("Dla krawędzi", e)
+    #     # DLa np ['A', 'B'] bierzemy wartość 'B' i
+    #     # musimy wziąc wszystkie krawędzie które kierują sie do 'B' i będziemy szukać czy dązą do 'A'
+    #     v = e[1]
+    #     temp_list = []
+    #     for e1 in graph:
+    #         if e1[1] == v:
+    #             # Tworze pomocniczą listę krawedzi wchodzacych do 'B'
+    #             temp_list.append(e1)
+    #
+    #     for e1 in temp_list:
+    #         v1 = e1[0]
+    #         v2 = e1[1]
+    #         if e1 == e:
+    #             temp_list.remove(e1)
+    #         else:
+    #             boolean = True
+    #             while boolean:
+    #                 for e2 in graph:
+    #                     if v in e2[1]:
+    #                         return_list.append([e2, e1, e])
+    #                     if v1 in e2[1]:
+    #
+    #
+        #
+        # for e1 in temp_list:
+        #     if
+        # print(temp_list)
+
+    import networkx as nx
+
+    G = nx.DiGraph(graph)
+
+    for cycle in nx.simple_cycles(G):
+        print(f"Pętla {cycle}")
+
+
+def zadanie7(graph):
     """Funkcja pokazujaca wszystkie krawedzie dwukierunkowe"""
 
     all_edges = []
 
-    for e in graf:
+    for e in graph:
         er = [e[1], e[0]]  # 'er' edge reversed - Krawedź odwrotna
-        if er in graf:
+        if er in graph:
             all_edges.append(f"Krawędź {e} i {er}")
 
     print(all_edges)
@@ -212,7 +303,11 @@ if __name__ == '__main__':
     zadanie2(graf2)
     print("Zadanie 3")
     zadanie3(graf2)
-
-
+    print("Zadanie 4")
+    zadanie4(graf2)
+    print("Zadanie 5")
+    zadanie5(graf4)
+    print("Zadanie 6")
+    zadanie6(graf5)
     print("Zadanie 7")
     zadanie7(graf5)
